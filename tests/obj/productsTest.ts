3b953476b381304bed192b6981a4344b7eebf621
JaraPava <alberto-11111@live.com.mx>
2024-04-09 16:24:05 -0600

    Se limpia la carpeta de testing

--- tests/obj/productsTest.ts
+++ tests/obj/productsTest.ts
@@ -1,4 +1,4 @@
-import { Products } from '../../src/data/interfaces/products.interface';
+import { Products } from '../../src/data/interfaces/products';
 import productsToEdit from './productsToEdit.json';
 
 export const products:Products = JSON.parse(JSON.stringify(productsToEdit));
--- tests/obj/userTest.ts
+++ tests/obj/userTest.ts
@@ -1,5 +1,5 @@
 
-import { User } from '../../src/data/interfaces/users.interface'
+import { User } from '../../src/data/interfaces/users'
 import usersTest  from '../../tests/obj/userTest.json';
 
 export const users:User = JSON.parse(JSON.stringify(usersTest));
