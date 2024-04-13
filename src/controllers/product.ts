76c9c9afceca066b4d395d3d62586a77640d6b64
JaraPava <alberto-11111@live.com.mx>
2024-04-12 22:29:32 -0600

    Se crea cascarón http en los controladores

---
+++ src/controllers/product.ts
@@ -0,0 +1,41 @@
+import {Request, Response} from 'express';
+
+const getProduct = (req:Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const getProducts = (req:Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const postProduct = (req:Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const updateProduct = (req:Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const deleteProduct = (req:Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
---
+++ src/controllers/user.ts
@@ -0,0 +1,40 @@
+import {Request, Response} from 'express'
+const getUser = (req:Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const getUsers = (req:Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const postUser = (req:Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const updateUser = (req:Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const deleteUser = (req:Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
