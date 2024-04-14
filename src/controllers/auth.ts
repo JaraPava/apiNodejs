6b7550fe9622dbdf68756ec767365c60f17c4120
JaraPava <alberto-11111@live.com.mx>
2024-04-14 14:12:31 -0600

    Se agrega también correctamente el login controller llamado aut, creación del  servicio del auth, route del auth y tambien la interface

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,8 @@
-
+import {Request, Response} from 'express';
 const registerCtrl = async(req:Request,res:Response) =>{
     try {
         
+        res.send(req)
     } catch (error) {
         
     }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express';
+import { loginCtrl, registerCtrl } from '../controllers/auth';
 
 const router = Router();
 
 
-router.post("/register");
-router.post("/login");
+router.post("/register", registerCtrl);
+router.post("/login", loginCtrl);
 export {router};
